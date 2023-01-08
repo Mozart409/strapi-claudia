0e4cfd217ffeb836724906efbb0374ca04d6f607
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-01-08 13:28:26 +0100

    fix: :fire: remove email plugin

--- backend/config/env/development/plugins.js
+++ backend/config/env/development/plugins.js
@@ -1,14 +1,4 @@
 module.exports = ({ env }) => ({
   // ...
   // ...
-  email: {
-    provider: "sendgrid",
-    providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"),
-    },
-    settings: {
-      defaultFrom: env("DEFAULT_FROM_EMAIL"),
-      defaultReplyTo: env("DEFAULT_REPLAY_EMAIL"),
-    },
-  },
 });
